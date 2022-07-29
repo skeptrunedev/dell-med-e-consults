17f0d188ea53d183505c19d908d03ed82346221e
Nick K <nicholas.khami@gmail.com>
2022-07-29 04:38:05 -0500

    reformat credits

--- components/app_shell/Footer.tsx
+++ components/app_shell/Footer.tsx
@@ -7,7 +7,10 @@ const  Footer: NextPage = () => {
     <footer className="bg-white">
       <div className="w-full mx-auto py-12 px-6">
         <div className="mt-8">
-          <p className="w-full text-center text-base text-gray-400">&copy; Created by <a href="https://nicholaskhami.site/">Nicholas Khami</a>, designed by .</p>
+          <p className="w-full text-center text-base text-gray-400">
+          Property of The University of Texas at Austin.  Designed by Divya Mirchandani and developed by
+          Created by <a href="https://nicholaskhami.site/">Nicholas Khami</a>, at the SAGA Lab .
+          </p>
         </div>
       </div>
     </footer>
