7e009cadb5d9848f4c3f07ec4a38141bfa22965b
Nicholas Khami <nicholas.khami@gmail.com>
2022-08-10 09:46:12 -0500

    decision tree formatting fix

---
+++ pages/home/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+import Navbar from '../../components/app_shell/Navbar'
+import Footer from '../../components/app_shell/Footer'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Navbar { ...{ active_page: 'About The Tool' } }/>
+      <Footer />
+    </div>
+  )
+}
+
+export default Home
