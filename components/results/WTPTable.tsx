ea668af5d8d13f99444e5c50d5b6f70bf34377cc
Nicholas Khami <nicholas.khami@gmail.com>
2022-08-14 03:39:17 -0500

    fix standout number

--- components/results/WTPTable.tsx
+++ components/results/WTPTable.tsx
@@ -34,91 +34,100 @@ const  WTPTable: NextPage = () => {
   } , [wtpPerEConsultation, totalCostPerEConsultation]);
 
   return (
-    <div className="mt-16 mx-4 md:mx-28 text-casal-400">
-      <span className="text-2xl font-semibold"> Calculated Results </span>
-      <div className="grid px-6 py-6 mt-6 border rounded-xl border border-casal-300">
-        <div className="flex justify-between">
-          <div className="text-lg font-semibold self-center">
-            <span> Willingness to pay (WTP) per e-consult </span>
-          </div>
-        </div>
-        <div className="grid mt-6 border-t border-casal-300">
-          {/* Row */}
-          <div className="grid grid-cols-2 mt-6 font-medium">
-            <div>
-              Average cost per e-consult
-            </div>
-            <span>
-              $ { averageCostPerEConsultation }
-            </span>
-          </div>
-          {/* Row */}
-          <div className="grid grid-cols-2 mt-6 font-medium">
-            <div>
-              Average cost per usual care
-            </div>
-            <span>
-              $ { averageCostPerUsualCare}
-            </span>
-          </div>
-          {/* Row */}
-          <div className="grid grid-cols-2 mt-6 font-medium">
-            <div>
-              Average savings per e-consult visit
-            </div>
-            <span>
-              $ { averageSavingsPerEConsultation }
-            </span>
-          </div>
-          {/* Row */}
-          <div className="grid grid-cols-2 mt-6 font-medium">
-            <div>
-              Total cost per e-consult
-            </div>
-            <span>
-              $ {totalCostPerEConsultation}
-            </span>
-          </div>
-          {/* Row */}
-          <div className="grid grid-cols-2 mt-6 font-medium">
-            <div>
-              Cost per e-consult averaged over program
-            </div>
-            <span>
-              $ { costPerEConsultationAeragedOverProgram }
-            </span>
-          </div>
-          {/* Row */}
-          <div className="grid grid-cols-2 mt-6 font-medium">
-            <div>
-              WTP per e-consult
-            </div>
-            <span>
-              $ { wtpPerEConsultation }
-            </span>
-          </div>
-          {/* Row */}
-          <div className="grid grid-cols-2 mt-6 font-medium">
-            <div>
-              Loss/Gain per e-consult
-            </div>
-            <span>
-              $ { lossGainPerEConsultation }
-            </span>
-          </div>
+    <div>
+      <div className="grid grid-cols-1 mt-14 px-4 md:px-28">
+        <div className="text-center">
+          <p className={"text-2xl font-semibold " + (Number(lossGainPerEConsultation) > 0 ? 'text-green-500' : 'text-red-600') }> $ { lossGainPerEConsultation }</p>
+          <p className=""> Provider Average cost/savings e-consult </p>
         </div>
       </div>
-      <div className="flex justify-end items-center mt-20">
-        <a className="mr-6 cursor-pointer" href="/parameters">
-          Go Back
-        </a>
-        <button
-          type="button"
-          className="self-center w-fit h-fit px-16 py-4 font-medium rounded-xl text-white text-xl bg-casal-400 hover:bg-casal-400/80 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          onClick={() => { window.print() }}
-        >
-          Print
-        </button>
+    
+      <div className="mt-16 mx-4 md:mx-28 text-casal-400">
+        <span className="text-2xl font-semibold"> Calculated Results </span>
+        <div className="grid px-6 py-6 mt-6 border rounded-xl border border-casal-300">
+          <div className="flex justify-between">
+            <div className="text-lg font-semibold self-center">
+              <span> Willingness to pay (WTP) per e-consult </span>
+            </div>
+          </div>
+          <div className="grid mt-6 border-t border-casal-300">
+            {/* Row */}
+            <div className="grid grid-cols-2 mt-6 font-medium">
+              <div>
+                Average cost per e-consult
+              </div>
+              <span>
+                $ { averageCostPerEConsultation }
+              </span>
+            </div>
+            {/* Row */}
+            <div className="grid grid-cols-2 mt-6 font-medium">
+              <div>
+                Average cost per usual care
+              </div>
+              <span>
+                $ { averageCostPerUsualCare}
+              </span>
+            </div>
+            {/* Row */}
+            <div className="grid grid-cols-2 mt-6 font-medium">
+              <div>
+                Average savings per e-consult visit
+              </div>
+              <span>
+                $ { averageSavingsPerEConsultation }
+              </span>
+            </div>
+            {/* Row */}
+            <div className="grid grid-cols-2 mt-6 font-medium">
+              <div>
+                Total cost per e-consult
+              </div>
+              <span>
+                $ {totalCostPerEConsultation}
+              </span>
+            </div>
+            {/* Row */}
+            <div className="grid grid-cols-2 mt-6 font-medium">
+              <div>
+                Cost per e-consult averaged over program
+              </div>
+              <span>
+                $ { costPerEConsultationAeragedOverProgram }
+              </span>
+            </div>
+            {/* Row */}
+            <div className="grid grid-cols-2 mt-6 font-medium">
+              <div>
+                WTP per e-consult
+              </div>
+              <span>
+                $ { wtpPerEConsultation }
+              </span>
+            </div>
+            {/* Row */}
+            <div className="grid grid-cols-2 mt-6 font-medium">
+              <div>
+                Loss/Gain per e-consult
+              </div>
+              <span>
+                $ { lossGainPerEConsultation }
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-end items-center mt-20">
+          <a className="mr-6 cursor-pointer" href="/parameters">
+            Go Back
+          </a>
+          <button
+            type="button"
+            className="self-center w-fit h-fit px-16 py-4 font-medium rounded-xl text-white text-xl bg-casal-400 hover:bg-casal-400/80 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            onClick={() => { window.print() }}
+          >
+            Print
+          </button>
+        </div>
       </div>
     </div>
   )
--- pages/results/index.tsx
+++ pages/results/index.tsx
@@ -2,7 +2,6 @@ import type { NextPage } from 'next'
 import Navbar from '../../components/app_shell/Navbar'
 import Footer from '../../components/app_shell/Footer'
 import ResultsHeading from '../../components/results/ResultsHeading'
-import HighLevels from '../../components/results/HighLevels'
 import WTPTable from '../../components/results/WTPTable'
 
 const Results: NextPage = () => {
@@ -10,7 +9,6 @@ const Results: NextPage = () => {
     <div>
       <Navbar { ...{ active_page: 'Results' } }/>
       <ResultsHeading { ...{ active_page: 'Results' } }/>
-      <HighLevels />
       <WTPTable />
       <Footer />
     </div>
