4fd55c3e0b929e42b26ea6214f2026f3b47592c4
nicholas khami <nicholas.khami@gmail.com>
2022-11-13 03:53:36 -0600

    PWA debugging

--- components/app_shell/Navbar.tsx
+++ components/app_shell/Navbar.tsx
@@ -80,6 +80,8 @@ const  Navbar: NextPage<NavbarProps> = ({ active_page }) => {
         <meta property="og:image" content="https://www.econanalysis.io/static_screenshot.png" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#e3ff88" />
+        <meta name="mobile-web-app-capable" content="yes" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
