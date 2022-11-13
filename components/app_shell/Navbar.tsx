bfa66358def95a1602ef42fce2bdc8475b15f8df
nicholas khami <nicholas.khami@gmail.com>
2022-11-13 03:37:51 -0600

    add link for manifest

--- components/app_shell/Navbar.tsx
+++ components/app_shell/Navbar.tsx
@@ -79,6 +79,7 @@ const  Navbar: NextPage<NavbarProps> = ({ active_page }) => {
         <meta property="og:type" content="website" />
         <meta property="og:image" content="https://www.econanalysis.io/static_screenshot.png" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
