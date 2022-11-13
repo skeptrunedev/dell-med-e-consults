f144ef086a4eb4444135aa45490e5b6f3ee9e0de
nicholas khami <nicholas.khami@gmail.com>
2022-11-13 02:49:08 -0600

    params tooltips

--- components/parameters/payer_cost_parameters/amount_for_e_consults.tsx
+++ components/parameters/payer_cost_parameters/amount_for_e_consults.tsx
@@ -1,105 +1,134 @@
-
-import type { NextPage } from 'next'
-import { GetExpandedAll } from '../../../interfaces/parameters';
-import { useEffect, useState } from 'react';
-import { ChevronUpIcon, ChevronDownIcon } from '@heroicons/react/outline';
-import AmountForEConsultsTable from './amount_for_econsults.table';
-import LargeInput from '../../util/large-input';
-import { Code } from '../../../interfaces/tables';
+import type { NextPage } from "next";
+import { GetExpandedAll } from "../../../interfaces/parameters";
+import { useEffect, useState } from "react";
+import { ChevronUpIcon, ChevronDownIcon } from "@heroicons/react/outline";
+import AmountForEConsultsTable from "./amount_for_econsults.table";
+import LargeInput from "../../util/large-input";
+import { Code } from "../../../interfaces/tables";
 
 const calculateAmountForEConsults = () => {
   let percentageOfTotalVisits = 0;
   let averagePhysicianTime = 0;
   let weightedCostPerVisit = 0;
-  const codes: Code[] = JSON.parse(window.localStorage.getItem('codes') || '[]');
-  codes.forEach(code => {
-    percentageOfTotalVisits += Number(code.percentage_of_total_visits || '0');
-    averagePhysicianTime += Number(code.avg_physician_time_spent || '0') * Number(code.percentage_of_total_visits || '0') / 100;
-    weightedCostPerVisit += Number(code.CMS_non_facility_price || '0') * Number(code.percentage_of_total_visits || '0')  / 100;
+  const codes: Code[] = JSON.parse(
+    window.localStorage.getItem("codes") || "[]"
+  );
+  codes.forEach((code) => {
+    percentageOfTotalVisits += Number(code.percentage_of_total_visits || "0");
+    averagePhysicianTime +=
+      (Number(code.avg_physician_time_spent || "0") *
+        Number(code.percentage_of_total_visits || "0")) /
+      100;
+    weightedCostPerVisit +=
+      (Number(code.CMS_non_facility_price || "0") *
+        Number(code.percentage_of_total_visits || "0")) /
+      100;
   });
   weightedCostPerVisit = Number(weightedCostPerVisit.toFixed(2));
   averagePhysicianTime = Number(averagePhysicianTime.toFixed(2));
   percentageOfTotalVisits = Number(percentageOfTotalVisits.toFixed(2));
-  window.localStorage.setItem('amountForEConsultsPercentageOfTotalVisits', percentageOfTotalVisits.toString());
-  window.localStorage.setItem('amountForEConsultsAveragePhysicianTime', averagePhysicianTime.toString());
-  window.localStorage.setItem('averageTimeSpentOnEConsultByDoctor', averagePhysicianTime.toString());
-  window.localStorage.setItem('amountForEConsultsWeightedCostPerVisit', weightedCostPerVisit.toString());
+  window.localStorage.setItem(
+    "amountForEConsultsPercentageOfTotalVisits",
+    percentageOfTotalVisits.toString()
+  );
+  window.localStorage.setItem(
+    "amountForEConsultsAveragePhysicianTime",
+    averagePhysicianTime.toString()
+  );
+  window.localStorage.setItem(
+    "averageTimeSpentOnEConsultByDoctor",
+    averagePhysicianTime.toString()
+  );
+  window.localStorage.setItem(
+    "amountForEConsultsWeightedCostPerVisit",
+    weightedCostPerVisit.toString()
+  );
 
-  return { percentageOfTotalVisits, averagePhysicianTime, weightedCostPerVisit };
-}
+  return {
+    percentageOfTotalVisits,
+    averagePhysicianTime,
+    weightedCostPerVisit,
+  };
+};
 
-const  AmountForEConsults: NextPage<GetExpandedAll> = ({expandAllSetting}) => {
+const AmountForEConsults: NextPage<GetExpandedAll> = ({ expandAllSetting }) => {
   const [expanded, setExpanded] = useState(false);
-  const [weightedCostPerVisit, setWeightedCostPerVisit] = useState('');
-  const [averageTimeSpent, setAverageTimeSpent] = useState('');
+  const [weightedCostPerVisit, setWeightedCostPerVisit] = useState("");
+  const [averageTimeSpent, setAverageTimeSpent] = useState("");
 
   useEffect(() => {
-    setExpanded(expandAllSetting == 'expanded');
+    setExpanded(expandAllSetting == "expanded");
   }, [expandAllSetting]);
 
   useEffect(() => {
     const handleStorageEvent = () => {
-      const { weightedCostPerVisit, averagePhysicianTime } = calculateAmountForEConsults();
+      const { weightedCostPerVisit, averagePhysicianTime } =
+        calculateAmountForEConsults();
       setWeightedCostPerVisit(String(weightedCostPerVisit));
       setAverageTimeSpent(String(averagePhysicianTime));
-    }
+    };
     handleStorageEvent();
 
-    window.addEventListener('amountForEConsults', handleStorageEvent);
+    window.addEventListener("amountForEConsults", handleStorageEvent);
 
-    return () => window.removeEventListener('amountForEConsults', handleStorageEvent);
+    return () =>
+      window.removeEventListener("amountForEConsults", handleStorageEvent);
   }, []);
 
   const displayExpandedTable = () => {
-    if(expanded) {
-      return (
-        <AmountForEConsultsTable />
-      )
+    if (expanded) {
+      return <AmountForEConsultsTable />;
     }
-  }
+  };
 
   return (
-    <div className="grid mt-4 mx-4 px-6 py-6 md:mx-28 border rounded-xl border-casal-300">
+    <div className="mx-4 mt-4 grid rounded-xl border border-casal-300 px-6 py-6 md:mx-28">
       <div className="flex justify-between">
-        <div className="text-lg font-semibold self-center">
-          <p> For E-consults </p>
-          <p className="text-sm font-normal"> Enter details of payor reimbursement for telemedicine visits </p>
-        </div>
-        <div className="flex space-x-4 col-span-2 justify-self-end">
-          <div className="grid grid-cols-1 md:grid-cols-2 md:space-x-4">
-          <LargeInput
-            {...{
-              label: 'Average amount of cost per visit ($)',
-              placeholder: '',
-              value: weightedCostPerVisit,
-              setValue: setWeightedCostPerVisit,
-              type: 'USD',
-              errored: false,
-              disabled: true
-            }}
-          />
-          <LargeInput
-            {...{
-              label: 'Average time spent (in minutes)',
-              placeholder: '',
-              value: averageTimeSpent,
-              setValue: setAverageTimeSpent,
-              type: 'fixed2',
-              errored: false,
-              disabled: true
-            }}
-          />
+        <div className="self-center text-lg font-semibold">
+          <div className="flex">
+            <p> For E-consults </p>
           </div>
-          <span className="h-6 w-6 ml-2 mt-2 self-center cursor-pointer" onClick={() => setExpanded(!expanded)}>
+          <p className="text-sm font-normal">
+            {" "}
+            Enter details of payor reimbursement for telemedicine visits{" "}
+          </p>
+        </div>
+        <div className="col-span-2 flex space-x-4 justify-self-end">
+          <div className="grid grid-cols-1 md:grid-cols-2 md:space-x-4">
+            <LargeInput
+              {...{
+                label: "Average amount of cost per visit ($)",
+                placeholder: "",
+                value: weightedCostPerVisit,
+                setValue: setWeightedCostPerVisit,
+                type: "USD",
+                errored: false,
+                disabled: true,
+              }}
+            />
+            <LargeInput
+              {...{
+                label: "Average time spent (in minutes)",
+                placeholder: "",
+                value: averageTimeSpent,
+                setValue: setAverageTimeSpent,
+                type: "fixed2",
+                errored: false,
+                disabled: true,
+              }}
+            />
+          </div>
+          <span
+            className="ml-2 mt-2 h-6 w-6 cursor-pointer self-center"
+            onClick={() => setExpanded(!expanded)}
+          >
             {expanded ? <ChevronDownIcon /> : <ChevronUpIcon />}
           </span>
         </div>
       </div>
-      <div>
-        { displayExpandedTable() }
-      </div>
+      <div>{displayExpandedTable()}</div>
     </div>
-  )
-}
+  );
+};
 
 export default AmountForEConsults;
--- components/parameters/payer_cost_parameters/payer_cost_parameters_heading.tsx
+++ components/parameters/payer_cost_parameters/payer_cost_parameters_heading.tsx
@@ -1,23 +1,39 @@
+import type { NextPage } from "next";
+import { PayerCostParametersHeadingProps } from "../../../interfaces/parameters";
+import { InformationCircleIcon } from "@heroicons/react/outline";
 
-import type { NextPage } from 'next'
-import { PayerCostParametersHeadingProps } from '../../../interfaces/parameters';
-
-
-const  PayerCostParametersHeading: NextPage<PayerCostParametersHeadingProps> = ({ setExpandAllSetting }) => {
+const PayerCostParametersHeading: NextPage<PayerCostParametersHeadingProps> = ({
+  setExpandAllSetting,
+}) => {
   return (
-    <div className="mt-8 px-4 md:px-28 text-casal-400">
+    <div className="mt-8 px-4 text-casal-400 md:px-28">
       <div className="grid grid-cols-2">
-        <div className='flex'>
-          <span className="text-2xl font-semibold"> Payor Cost </span>
+        <div className="flex">
+          <span className="text-2xl font-semibold"> Payor Costs </span>
+          <a href="/faq#payor-costs">
+            <InformationCircleIcon className="ml-2 mt-1 h-6 w-6 hover:cursor-pointer hover:text-casal-600" />
+          </a>
         </div>
-        <div className="flex self-center text-sm md:text-base mt-2 justify-self-end mr-6 select-none">
-          <span className="cursor-pointer" onClick={() => setExpandAllSetting('expanded')}> Expand All </span>
+        <div className="mt-2 mr-6 flex select-none self-center justify-self-end text-sm md:text-base">
+          <span
+            className="cursor-pointer"
+            onClick={() => setExpandAllSetting("expanded")}
+          >
+            {" "}
+            Expand All{" "}
+          </span>
           <span className="px-3"> | </span>
-          <span className="cursor-pointer" onClick={() => setExpandAllSetting('collapsed')}> Collapse All </span>
+          <span
+            className="cursor-pointer"
+            onClick={() => setExpandAllSetting("collapsed")}
+          >
+            {" "}
+            Collapse All{" "}
+          </span>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default PayerCostParametersHeading;
